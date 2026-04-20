2dd28053f26ae3a3533063480c2d355bf42e8397
nikhil15-gif <nikhilashok159@gmail.com>
2026-04-20 16:25:06 +0530

    NODEJS

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const http = require('http'); 
+const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    res.end("Hello World");
+    if (req.url === "/") {
+        res.end("hello world");
+    }
+    else if (req.url === "/contact") {
+        res.end("Contact page")
+    }
+    else if (req.url === "/about") {
+        res.end("About page")
+    }
+    else{
+        res.end("Page not found");
+    }
 });
-
 server.listen(3001, () => {
     console.log("Server is listening on port 3001");
 });
