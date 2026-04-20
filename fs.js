c722f608e4f789089689f9ea71ff96dd903afaed
nikhil15-gif <nikhilashok159@gmail.com>
2026-04-20 14:19:43 +0530

    NODEJS

--- fs.js
+++ fs.js
@@ -1,25 +1,25 @@
  const fs = require('fs');
 
-// fs.mkdir("docs", (err) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log("Directory created successfully!");
-//     }
+fs.mkdir("docs", (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Directory created successfully!");
+    }
     
-// });
-//fs.writeFile("docs/info.txt", "Hello, World!", (err) => {
-  //  if (err) {
-    //    console.log(err);
-    //} 
-  //  console.log("File created successfully!");
-//})
-// fs.readFile("docs/info.txt", "utf8", (err, data) => {
-//     if (err) {
-//         console.log(err.message);
-//     } else{
-//         console.log(data);
-// }
+});
+fs.writeFile("docs/info.txt", "Hello, World!", (err) => {
+   if (err) {
+       console.log(err);
+    } 
+   console.log("File created successfully!");
+})
+fs.readFile("docs/info.txt", "utf8", (err, data) => {
+    if (err) {
+        console.log(err.message);
+    } else{
+        console.log(data);
+}
     
 // })
 if (fs.existsSync("docs/info.txt")) {
